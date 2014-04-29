a4936a5554e67bbd1e56a5757b27e0f9adf6c9c3
Cristina Aurrecoechea <aurreco@uga.edu>
2014-04-29 00:27:45 +0000

    closes #14444
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/GBrowse/trunk@61826 735e2a04-f8fc-0310-8a1b-f2942603c481

--- src/GBrowse/conf/languages/en.pm
+++ src/GBrowse/conf/languages/en.pm
@@ -210,7 +210,7 @@ END
 
    UPLOAD_TITLE=> 'Upload your own data',
 
-   UPLOAD_FILE => 'Upload a track file',
+   UPLOAD_FILE => 'Upload a track file; this might take some time depending on the size and format; see different options at <a target="_blank" href="http://eupathdb.org/tutorials/GBrowse-UploadTrack-Help.pdf">EuPathDB specific help</a>.<br><br>',
 
    MIRROR_FILE  => 'Fetch track file from this URL',
 
